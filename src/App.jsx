5ae5743f9ac1e0eda3e782e0f41a1ae0f790bdb1
MartinJindu <martinchijindu@gmail.com>
2025-06-23 22:05:10 +0100

    implement cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 // registering plugin across the appilication
 // scrollTrigger allow to animate thing base on scroll position
@@ -13,8 +14,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      {/* test hero leaf animation */}
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
     </main>
   );
 };
