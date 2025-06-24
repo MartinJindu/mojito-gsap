844226b17f2c11fec4e517b333c8708c4e2c90c2
MartinJindu <martinchijindu@gmail.com>
2025-06-24 21:43:10 +0100

    implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 // registering plugin across the appilication
 // scrollTrigger allow to animate thing base on scroll position
@@ -15,6 +16,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
